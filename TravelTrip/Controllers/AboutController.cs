ca02a7ef9e4332464cb109eba2de373fb8cd707a
OrkunCeyhan <77159080+OrkunCeyhan@users.noreply.github.com>
2022-11-15 14:28:21 +0300

    Add project files.

---
+++ TravelTrip/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Sınıflar;
+namespace TravelTrip.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context(); //contex isimli sınıftan c isimli nesne türettik 
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList(); // c neesnesi yardımıylada contexte bağlı olan sınıflar içinde hakkımızda tablosuna ulasıp bu tabloyu listele dedik
+            return View(degerler);
+        }
+    }
+}
---
+++ TravelTrip/Models/Sınıflar/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTrip.Models.Sınıflar
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string Kullanici { get; set; }
+        public string Sifre { get; set; }
+    }
+}
